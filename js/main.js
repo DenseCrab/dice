bff56c08584ad61a392d1c9444b8c493180087a1
DenseCrab <seppevolders3@hotmail.com>
2016-10-18 15:53:28 +0200

    js adding chat tab

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var chat_sock = new WebSocket('ws://localhost:9002');
 
-function htmlEncode(value){
-  //create a in-memory div, set it's inner text(which jQuery automatically encodes)
-  //then grab the encoded contents back out.  The div never exists on the page.
-  return $('<div/>').text(value).html();
+function htmlEncode(value) {
+    //create a in-memory div, set it's inner text(which jQuery automatically encodes)
+    //then grab the encoded contents back out.  The div never exists on the page.
+    return $('<div/>').text(value).html();
 }
 
-function htmlDecode(value){
-  return $('<div/>').html(value).text();
+function htmlDecode(value) {
+    return $('<div/>').html(value).text();
 }
 
 function get_active_chat_tab() {
@@ -21,7 +21,7 @@ function get_active_chat_tab() {
 }
 
 function chat_submit() {
-	// $("#server-information .table").append(make_chat_entry("DenseCrab", "Test NEW UI",""));
+    // $("#server-information .table").append(make_chat_entry("DenseCrab", "Test NEW UI",""));
     var input = $("#btn-input").val();
     if (input === "")
         return;
@@ -41,7 +41,7 @@ $("#btn-input").bind('keypress',
     }
 );
 
-chat_sock.onopen = function(){
+chat_sock.onopen = function() {
     console.log("Connection established");
 
     var username = localStorage.getItem('username');
@@ -57,8 +57,8 @@ chat_sock.onopen = function(){
 
 function make_chat_entry(header, body, time) {
     var item = '<tr class="chat-entry"><td class="time">' + (time !== undefined ? time : '') + '</td>' +
-	'<td class="usr">' + header + '</td>' +
-	'<td class="msg">' + body + '</td></tr>';
+        '<td class="usr">' + header + '</td>' +
+        '<td class="msg">' + body + '</td></tr>';
     return item;
 }
 
@@ -68,7 +68,7 @@ function make_chat_tab(channel) {
 }
 
 function make_chat_body(channel) {
-    var item = '<div id="chat-' + channel + '" class="tab-pane fade"><ul class="chat"></ul></div>';
+    var item = '<div id="chat-' + channel + '" class="tab-pane fade"> <div class="col-xs-8 chatcol"><table class="table"></table></div><div class="col-xs-4 userscol"></div></div>';
     return item;
 }
 
@@ -93,16 +93,16 @@ chat_sock.onmessage = function(evt) {
         }
 
         var channel = obj["join"]["channel"];
-        server_info.append(make_chat_entry("", 'You have joined ' + channel)); 
+        server_info.append(make_chat_entry("", 'You have joined ' + channel));
         channel = channel.substr(1, channel.length);
 
-	var chat_tab = make_chat_tab(channel);
+        var chat_tab = make_chat_tab(channel);
         var chat_body = make_chat_body(channel);
 
         $(".nav.nav-tabs").append(chat_tab);
         $(".tab-content").append(chat_body);
     } else if (obj["send"]) {
-	var success = obj["send"]["success"] === true;
+        var success = obj["send"]["success"] === true;
 
         server_info.append(make_chat_entry("", "send " + (success ? "success" : "failure")));
     } else if (obj["msg"]) {
@@ -128,35 +128,39 @@ chat_sock.onmessage = function(evt) {
 //Add chat-tab
 
 //remove panel footer on click
-$("a[href='#add-tab']").on("click",function(){
-	$("#chatcol .panel-footer").addClass("invisible");
+$("a[href='#add-tab']").on("click", function() {
+    $("#chatcol .panel-footer").addClass("invisible");
 });
 
-$("a[href!='#add-tab']").on("click",function(){
-	$("#chatcol .panel-footer").removeClass("invisible");
+$("a[href!='#add-tab']").on("click", function() {
+    $("#chatcol .panel-footer").removeClass("invisible");
 });
-$("#add-tab #add-tab-btn").on("click",function(){
-	//add tab
-});
-$("#add-tab #usrname").on("keyup", function(event){
-	if($(this).val() !== "")
-		$("#add-tab #add-user").removeClass("disabled");
+
+$("#add-tab #usrname").on("keyup", function(event) {
+    if ($(this).val() !== "")
+        $("#add-tab #add-user").removeClass("disabled");
     else
-		$("#add-tab #add-user").addClass("disabled");
+        $("#add-tab #add-user").addClass("disabled");
 });
 
 $("#add-tab #usrname").keypress(function(e) {
-    if(e.which == 13) 
+    if (e.which == 13)
         $("#add-tab #add-user").trigger("click");
 });
 
-$("#add-tab #add-user").on("click", function(){
-	if(!$(this).hasClass("disabled")){
-		var usernameinput = $("#add-tab #usrname");
+$("#add-tab #add-user").on("click", function() {
+    if (!$(this).hasClass("disabled")) {
+        var usernameinput = $("#add-tab #usrname");
 
-	$("#add-tab #added-users").prepend('<a href="#" class="list-group-item">' + usernameinput.val() +'</a>');
-	usernameinput.val("").focus();
-}
+        $("#add-tab #added-users").prepend('<a href="#" class="list-group-item">' + usernameinput.val() + '</a>');
+        usernameinput.val("").focus();
+    }
+});
+
+//make-chat-tab
+$('#add-tab #add-tab-btn').on('click', function() {
+    $('#chatcol-chat .nav-tabs #add-tab-tab').before(make_chat_tab($('#add-tab #chat-name').val()));
+    $('#chatcol-chat .tab-content').append(make_chat_body($('#add-tab #chat-name').val()));
 });
 
 
@@ -175,43 +179,43 @@ $("#arrow").on("click", function() {
 });
 
 //MINMAXSHIT
-$("#btnsbet #max").on("click", function(){
-	$("#betcol #bet").val($("#betcol #bal").html());
+$("#btnsbet #max").on("click", function() {
+    $("#betcol #bet").val($("#balcol #bal").html());
 });
 
-$("#btnsbet #min").on("click", function(){
-	$("#betcol #bet").val("0.00000000");
+$("#btnsbet #min").on("click", function() {
+    $("#betcol #bet").val("0.00000000");
 });
 
-$("#btnsbet #half").on("click", function(){
-	$("#betcol #bet").val(($("#betcol #bet").val()/2).toFixed(8));
+$("#btnsbet #half").on("click", function() {
+    $("#betcol #bet").val(($("#betcol #bet").val() / 2).toFixed(8));
 });
 
-$("#btnsbet #double").on("click", function(){
-	$("#betcol #bet").val(($("#betcol #bet").val()*2).toFixed(8));
+$("#btnsbet #double").on("click", function() {
+    $("#betcol #bet").val(($("#betcol #bet").val() * 2).toFixed(8));
 });
 
 
 //Winchance - Multiplier 
-var edge = 1/100;
+var edge = 1 / 100;
 
 $("#betcol #multiplier").numeric();
 $("#betcol #winchance").numeric();
 
-$("#betcol #winchance").on("keyup", function(){
-	$("#betcol #multiplier").val(calculateWinchanceMultiplier($(this).val()));	
+$("#betcol #winchance").on("keyup", function() {
+    $("#betcol #multiplier").val(calculateWinchanceMultiplier($(this).val()));
 });
 
-$("#betcol #multiplier").on("keyup", function(){
-	$("#betcol #winchance").val(calculateWinchanceMultiplier($(this).val()));	
+$("#betcol #multiplier").on("keyup", function() {
+    $("#betcol #winchance").val(calculateWinchanceMultiplier($(this).val()));
 });
 
-function calculateWinchanceMultiplier(input){
-	var wm = 100/input; 
-	var edgedwm = (wm - wm*edge).toFixed(2);
-	console.log(wm);
-	console.log(edgedwm);
-	return edgedwm;
+function calculateWinchanceMultiplier(input) {
+    var wm = 100 / input;
+    var edgedwm = (wm - wm * edge).toFixed(2);
+    console.log(wm);
+    console.log(edgedwm);
+    return edgedwm;
 }
 
 //table bets
@@ -232,4 +236,3 @@ $("#betcol #roll").on("click", function() {
         "</tr>"
     )
 });
-
